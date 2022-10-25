5ebb0f0715c0e86ff40af7b9f3c6da5baf489b9c
Fauzi kurniawan <ozilpym123@gmail.com>
2022-10-25 19:50:55 +0700

    revisi main

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 """
 Aplikasi Berita Terpopuler Detikcom
 """
-from beritaterkini import ekstraksi_data, tampilkan_data
+
+import beritaterkini
 
 if __name__ == "__main__":  # -> kode hanya akan di eksekusi jika sebagai module utama bukan dari perintah import
     print("Berita Terpopuler dan Terkini Detik.com\n")
+    result = beritaterkini.ekstraksi_data()
+    beritaterkini.tampilkan_data(result)
+
+# Alternatif
+# cara ini kurang, kita tidak tahu ektraksi data dari package yang mana
+
+"""
+from beritaterkini import ekstraksi_data, tampilkan_data
+
+if __name__ == "__main__":  
+    print("Berita Terpopuler dan Terkini Detik.com\n")
     result = ekstraksi_data()
     tampilkan_data(result)
+"""
+
