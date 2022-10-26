7bfade48fb814c41feb3761c3340ede86f74c810
Fauzi kurniawan <ozilpym123@gmail.com>
2022-10-26 16:46:18 +0700

    import package

--- beritaterkini/__init__.py
+++ beritaterkini/__init__.py
@@ -1,23 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 def ekstraksi_data():
-    """
-    Berita Terpopuler
-#1
-Tangis Vera Saat Ungkap Yosua Curhat Masalah yang Ditanggung Sendiri
-detikNews | 1 jam yang lalu
-#2
-Erick Thohir: Yang Jadi Presiden Pasti Bukan Saya, Berikutnya Orang Jawa
-detikFinance | 1 jam yang lalu
-#3
-Penampakan Wanita Coba Terobos Istana Kini Diborgol di Kantor Polisi
-detikNews | 2 jam yang lalu
-#4
-Adik Yosua: Saya Dilarang Kombes Pakaikan Baju dan Gendong Jenazah Kakak
-detikNews | 45 menit yang lalu
-#5
-Rizaldi Sempat Jadi 'Pak Ogah' Usai Bunuh Anak Perempuan Pulang Ngaji
-detikNews | 1 jam yang lalu
-    :return:
-    """
+    try:
+        page = requests.get('http://detik.com')
+    except Exception:
+        return None
+    if page.status_code == 200:
+        soup = BeautifulSoup(page.text, 'html.parser')
+        print(soup.prettify())
+
     hasil = dict()  # perintah/cara lain agar otomatis mengubah menjadi tipe data dictionary
     hasil['pertama'] = {
         'isi': 'Tangis Vera Saat Ungkap Yosua Curhat Masalah yang Ditanggung Sendiri',
@@ -44,8 +37,17 @@ detikNews | 1 jam yang lalu
 
 
 def tampilkan_data(result):
+    if result is None:
+        print('Tidak dapat menemukan data berita terkini')
+        return None
     print(f"#1. {result['pertama']['isi']}. \n   Diupload {result['pertama']['diupload']}")
     print(f"#2. {result['kedua']['isi']}. \n   Diupload {result['kedua']['diupload']}")
     print(f"#3. {result['ketiga']['isi']}. \n   Diupload {result['ketiga']['diupload']}")
     print(f"#4. {result['keempat']['isi']}. \n   Diupload {result['keempat']['diupload']}")
     print(f"#5. {result['kelima']['isi']}. \n   Diupload {result['kelima']['diupload']}")
+
+
+if __name__ == "__main__":
+    print("Berita Terpopuler dan Terkini Detik.com\n")
+    result = ekstraksi_data()
+    tampilkan_data(result)
