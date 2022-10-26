fdca8e11fca03f108dbeb9c40a8d5c5954c56998
Fauzi kurniawan <ozilpym123@gmail.com>
2022-10-26 12:45:25 +0700

    revisi main

--- main.py
+++ main.py
@@ -12,12 +12,11 @@ if __name__ == "__main__":  # -> kode hanya akan di eksekusi jika sebagai module
 # Alternatif
 # cara ini kurang, kita tidak tahu ektraksi data dari package yang mana
 
-"""
-from beritaterkini import ekstraksi_data, tampilkan_data
 
-if __name__ == "__main__":  
-    print("Berita Terpopuler dan Terkini Detik.com\n")
-    result = ekstraksi_data()
-    tampilkan_data(result)
-"""
+# from beritaterkini import ekstraksi_data, tampilkan_data
+#
+# if __name__ == "__main__":
+#     print("Berita Terpopuler dan Terkini Detik.com\n")
+#     result = ekstraksi_data()
+#     tampilkan_data(result)
 
