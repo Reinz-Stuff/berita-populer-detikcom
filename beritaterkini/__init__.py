ead2f72e79a6075fd8c164dff7057d02df3ac008
Fauzi kurniawan <ozilpym123@gmail.com>
2022-10-28 06:54:56 +0700

    fetch upload revision

--- beritaterkini/__init__.py
+++ beritaterkini/__init__.py
@@ -8,53 +8,53 @@ def ekstraksi_data():
     except Exception:
         return None
     if page.status_code == 200:
-        # Belum direvisi apakah sudah betul menunjukan berita populer berdasarkan ranking. Kemungkinan menunjukan
-        # id berita saja!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # Belum menemukan cara fetch tanggal upload karena tanggal berta menempel pada id berita,sedangkan yg
+        # di fetch di berita terpopuler adalah rannking yang bisa berubah2!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         soup = BeautifulSoup(page.text, 'html.parser')
 
-        first_news = soup.find('a', {'dtr-id': '6372097'})
+        first_news = soup.find('a', {'dtr-evt': 'box artikel terpopuler', 'dtr-idx': '1'})
         first_news = first_news.text.strip()
-        first_upload = soup.find('span', {'d-time': '1666848035'})
-        first_upload = first_upload.text.strip()
 
-        second_news = soup.find('a', {'dtr-id': '6372158'})
+        upload = soup.find('div', {'class': 'media'})
+        # print(upload)
+
+        second_news = soup.find('a', {'dtr-evt': 'box artikel terpopuler', 'dtr-idx': '2'})
         second_news = second_news.text.strip()
-        second_upload = soup.find('span', {'d-time': '1666849534'})
-        second_upload = second_upload.text
-
-        third_news = soup.find('a', {'dtr-id': '6372043'})
+        # second_upload = soup.find('span', {'d-time': '1666849534'})
+        # second_upload = second_upload.text
+        #
+        third_news = soup.find('a', {'dtr-evt': 'box artikel terpopuler', 'dtr-idx': '3'})
         third_news = third_news.text.strip()
-        third_upload = soup.find('span', {'d-time': '1666846941'})
-        third_upload = third_upload.text
-
-        fourth_news = soup.find('a', {'dtr-id': '6372175'})
+        # third_upload = soup.find('span', {'d-time': '1666846941'})
+        # third_upload = third_upload.text
+        #
+        fourth_news = soup.find('a', {'dtr-evt': 'box artikel terpopuler', 'dtr-idx': '4'})
         fourth_news = fourth_news.text.strip()
-        fourth_upload = soup.find('span', {'d-time': '1666849774'})
-        fourth_upload = fourth_upload.text
-
-        fifth_news = soup.find('a', {'dtr-id': '6372043'})
-        fifth_news = fifth_news
-        print(fifth_news)
+        # fourth_upload = soup.find('span', {'d-time': '1666849774'})
+        # fourth_upload = fourth_upload.text
+        #
+        fifth_news = soup.find('a', {'dtr-evt': 'box artikel terpopuler', 'dtr-idx': '5'})
+        fifth_news = fifth_news.text.strip()
 
         hasil = dict()
         hasil['first_news'] = {
             'isi': first_news,
-            'diupload': first_upload
+            'diupload': 'first_upload'
         }
-        hasil['kedua'] = {
+        hasil['second_news'] = {
             'isi': second_news,
-            'diupload': second_upload
+            'diupload': 'second_upload'
         }
-        hasil['ketiga'] = {
+        hasil['third_news'] = {
             'isi': third_news,
-            'diupload': third_upload
+            'diupload': 'third_upload'
         }
-        hasil['keempat'] = {
+        hasil['fourth_news'] = {
             'isi': fourth_news,
-            'diupload': fourth_upload
+            'diupload': 'fourth_upload'
         }
-        hasil['kelima'] = {
-            'isi': "Rizaldi Sempat \'Jadi Pak Ogah\' Usai Bunuh Anak Perempuan Pulang Ngaji",
+        hasil['fifth_news'] = {
+            'isi': fifth_news,
             'diupload': '1 jam yang lalu'
         }
 
@@ -66,10 +66,10 @@ def tampilkan_data(result):
         print('Tidak dapat menemukan data berita terkini')
         return None
     print(f"#1. {result['first_news']['isi']}. \n   Diupload {result['first_news']['diupload']}")
-    print(f"#2. {result['kedua']['isi']}. \n   Diupload {result['kedua']['diupload']}")
-    print(f"#3. {result['ketiga']['isi']}. \n   Diupload {result['ketiga']['diupload']}")
-    print(f"#4. {result['keempat']['isi']}. \n   Diupload {result['keempat']['diupload']}")
-    print(f"#5. {result['kelima']['isi']}. \n   Diupload {result['kelima']['diupload']}")
+    print(f"#2. {result['second_news']['isi']}. \n   Diupload {result['second_news']['diupload']}")
+    print(f"#3. {result['third_news']['isi']}. \n   Diupload {result['third_news']['diupload']}")
+    print(f"#4. {result['fourth_news']['isi']}. \n   Diupload {result['fourth_news']['diupload']}")
+    print(f"#5. {result['fifth_news']['isi']}. \n   Diupload {result['fifth_news']['diupload']}")
 
 
 if __name__ == "__main__":
